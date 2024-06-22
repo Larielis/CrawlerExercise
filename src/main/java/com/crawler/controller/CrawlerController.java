dd0ddc23a9a40aafc773c7810299a279cecfd473
Valerian <r.pino.camacho@gmail.com>
2024-06-22 15:06:03 +0200

    Added endpoints and views in controller

--- src/main/java/com/crawler/controller/CrawlerController.java
+++ src/main/java/com/crawler/controller/CrawlerController.java
@@ -4,7 +4,11 @@ import com.crawler.model.NewsEntry;
 import com.crawler.service.CrawlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class CrawlerController {
@@ -12,8 +16,23 @@ public class CrawlerController {
     private CrawlerService crawlerService;
 
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public ModelAndView home() {
+      return createModelAndView("home", crawlerService.fetchNewsEntries());
     }
 
+    @GetMapping("/longestTitles")
+    public ModelAndView longestTitles() {
+        return createModelAndView("longestTitles", crawlerService.filterLongTitles());
+    }
+
+    @GetMapping("/shortestTitles")
+    public ModelAndView shortestTitles() {
+        return createModelAndView("shortestTitles", crawlerService.filterShortTitles());
+    }
+
+    private ModelAndView createModelAndView(String viewName, List<NewsEntry> entries) {
+        ModelAndView modelAndView = new ModelAndView(viewName);
+        modelAndView.addObject("entries", entries);
+        return modelAndView;
+    }
 }
